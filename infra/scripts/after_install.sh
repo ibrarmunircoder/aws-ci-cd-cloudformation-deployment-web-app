3c42020fffbc4ffd1bc57ce0b3d5d7298943d95a
ibrarmunircoder <ibrarmunir009@gmail.com>
2023-10-07 15:19:01 +0500

    updated templates and scripts

--- infra/scripts/after_install.sh
+++ infra/scripts/after_install.sh
@@ -1,4 +1,9 @@
 if [[ "$DEPLOYMENT_GROUP_NAME" == "BenefitFormDeploymentGroup" ]]
 then
-    mv /tmp/benefit-form/build/* /var/www/benefit/
+    mv /tmp/benefit-form/build/* /var/www/benefit-form/
+fi
+
+if [[ "$DEPLOYMENT_GROUP_NAME" == "AdminPanelDeploymentGroup" ]]
+then
+    mv /tmp/admin-panel/build/* /var/www/admin-panel/
 fi
