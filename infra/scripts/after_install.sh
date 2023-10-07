2513f0fa81f354ef11c0c5f5a9131dd0a33bb8ac
ibrarmunircoder <ibrarmunir009@gmail.com>
2023-10-07 23:18:30 +0500

    added backend and Db instance template

--- infra/scripts/after_install.sh
+++ infra/scripts/after_install.sh
@@ -9,3 +9,13 @@ then
     rm -rf /var/www/admin-panel/*
     mv /tmp/admin-panel/build/* /var/www/admin-panel/
 fi
+
+if [[ "$DEPLOYMENT_GROUP_NAME" == "MailingBackendAppDeploymentGroup" ]]
+then
+    rm -rf /var/www/backend-app/*
+    mv /tmp/backend-app/* /var/www/backend-app/
+    npm --prefix /var/www/backend-app install
+    npm --prefix /var/www/backend-app run migration:up
+    npm --prefix /var/www/backend-app run build
+    npm --prefix /var/www/backend-app run start
+fi
