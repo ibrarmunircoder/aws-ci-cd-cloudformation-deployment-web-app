3ae5e6c76a213c67e5234e8aac23b466b5e8b838
ibrarmunircoder <ibrarmunir009@gmail.com>
2023-10-07 12:50:24 +0500

    updated scripts

--- infra/scripts/after_install.sh
+++ infra/scripts/after_install.sh
@@ -1,5 +1,4 @@
 if [[ "$DEPLOYMENT_GROUP_NAME" == "BenefitFormDeploymentGroup" ]]
 then
-    mkdir -p /var/www/benefit
-    mv /tmp/benefit/* /var/www/benefit/
+    mv /tmp/benefit-form/* /var/www/benefit/
 fi
