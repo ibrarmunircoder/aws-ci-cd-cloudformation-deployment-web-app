486022b6bc12482afcc06b25651c3bc4928cb253
ibrarmunircoder <ibrarmunir009@gmail.com>
2023-10-09 16:44:04 +0500

    added source

--- infra/scripts/after_install.sh
+++ infra/scripts/after_install.sh
@@ -14,7 +14,7 @@ if [[ "$DEPLOYMENT_GROUP_NAME" == "MailingBackendAppDeploymentGroup" ]]
 then
     rm -rf /var/www/backend-app/*
     mv /tmp/backend-app/* /var/www/backend-app/
-    /etc/profile 
+    source /etc/profile 
     npm --prefix /var/www/backend-app install
     npm --prefix /var/www/backend-app run migration:up
     npm --prefix /var/www/backend-app run build
